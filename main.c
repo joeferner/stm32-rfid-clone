5ef98a6a3f8fa0cd95c54afd40667f4d09efae0c
Joe Ferner <joe@fernsroth.com>
2013-12-10 00:17:12 -0500

    working rfid read

--- main.c
+++ main.c
@@ -21,6 +21,9 @@
 #define RF_RX_MAX_0              (RF_RX_CENTER_0 + RF_RX_CENTER_DIFF_DIV_2)
 #define RF_RX_MIN_1              (RF_RX_CENTER_1 - RF_RX_CENTER_DIFF_DIV_2)
 #define RF_RX_MAX_1              (RF_RX_CENTER_1 + RF_RX_CENTER_DIFF_DIV_2)
+#define RF_RX_0                  0
+#define RF_RX_1                  1
+#define RF_RX_UNKNOWN            2
 
 void setup();
 void loop();
@@ -34,6 +37,8 @@ void disable_jtag();
 void rf_rx_process_capture_buffer();
 uint16_t rf_rx_get(int i);
 void rf_rx_process(int i);
+void rf_rx_reset_read();
+void rf_rx_process_read_buffer();
 
 #define INPUT_BUFFER_SIZE 100
 uint8_t usbInputBuffer[INPUT_BUFFER_SIZE];
@@ -44,6 +49,16 @@ ring_buffer_u8 usbInputRingBuffer;
 #define RF_RX_CAPTURE_BUFFER_LEN 1000
 uint16_t rfRxCaptureBuffer[RF_RX_CAPTURE_BUFFER_LEN];
 volatile uint8_t rfRxCaptureBufferReady;
+
+#define READ_BUFFER_LEN 100
+#define READ_STATE_WAIT_FOR_START        0
+#define READ_STATE_WAIT_FOR_END_OF_START 1
+#define READ_STATE_DATA                  2
+#define READ_STATE_END                   3
+uint8_t readState;
+int8_t readCount; // >0 number of 1s. <0 number of 0s. 0 waiting for 0 or 1.
+uint16_t readBufferOffset;
+uint8_t readBuffer[READ_BUFFER_LEN];
 
 typedef struct {
   uint32_t start;
@@ -260,23 +275,104 @@ void rf_rx_process(int i) {
     return;
   }
 
+  int bit;
   if (t >= RF_RX_MIN_0 && t <= RF_RX_MAX_0) {
-    debug_write_line("0");
+    bit = RF_RX_0;
   } else if (t >= RF_RX_MIN_1 && t <= RF_RX_MAX_1) {
-    debug_write_line("1");
+    bit = RF_RX_1;
   } else {
-    if (t < RF_RX_MIN_0) {
-      debug_write_i32(t, 10);
-      debug_write_line("");
+    bit = RF_RX_UNKNOWN;
+  }
+
+  // we could be adding two bits to the read buffer
+  if (bit == RF_RX_UNKNOWN || readBufferOffset >= (READ_BUFFER_LEN - 2)) {
+    rf_rx_reset_read();
+    return;
+  }
+
+  switch (readState) {
+    case READ_STATE_WAIT_FOR_START:
+      if (bit == RF_RX_1) {
+        readCount++;
+        if (readCount >= 15) {
+          readState = READ_STATE_WAIT_FOR_END_OF_START;
+        }
+      } else {
+        readCount = 0;
+      }
+      break;
+
+    case READ_STATE_WAIT_FOR_END_OF_START:
+      if (bit == RF_RX_0) {
+        readState = READ_STATE_DATA;
+        readCount = 0;
+      } else {
+        break;
+      }
+      // NOTE: yes the lack of break here is intentional, if this is a data bit we need to process it.
+
+    case READ_STATE_DATA:
+      if (bit == RF_RX_0) {
+        if (readCount > 0) { // if last read is >0 it means the last bit was a one.
+          if (readCount >= 3 && readCount <= 8) {
+            readBuffer[readBufferOffset++] = 1;
+          } else if (readCount <= 14) {
+            readBuffer[readBufferOffset++] = 1;
+            readBuffer[readBufferOffset++] = 1;
+          } else {
+            readState = READ_STATE_END;
+            rf_rx_process_read_buffer();
+            return;
+          }
+          readCount = 0;
+        }
+        readCount--;
+      } else /* (bit == RF_RX_1) */ {
+        if (readCount < 0) { // if last read is <0 it means the last bit was a zero.
+          readCount = -readCount;
+          if (readCount >= 3 && readCount <= 8) {
+            readBuffer[readBufferOffset++] = 0;
+          } else if (readCount <= 14) {
+            readBuffer[readBufferOffset++] = 0;
+            readBuffer[readBufferOffset++] = 0;
+          } else {
+            readState = READ_STATE_END;
+            rf_rx_process_read_buffer();
+            return;
+          }
+          readCount = 0;
+        }
+        readCount++;
+      }
+      break;
+  }
+}
+
+void rf_rx_process_read_buffer() {
+  rf_rx_disable();
+  debug_write_line("");
+  for (uint16_t i = 0; i < readBufferOffset; i += 2) {
+    if (readBuffer[i] == 1 && readBuffer[i + 1] == 0) {
+      debug_write_ch('1');
+    } else if (readBuffer[i] == 0 && readBuffer[i + 1] == 1) {
+      debug_write_ch('0');
     } else {
-      debug_write_i32(t, 10);
-      debug_write_line("");
+      debug_write_ch('E');
     }
   }
+  debug_write_line("");
+  rf_rx_enable();
+}
+
+void rf_rx_reset_read() {
+  readState = READ_STATE_WAIT_FOR_START;
+  readCount = 0;
+  readBufferOffset = 0;
 }
 
 void rf_rx_enable() {
   debug_write_line("?rf_rx_enable");
+  rf_rx_reset_read();
   DMA_Cmd(DMA1_Channel2, ENABLE);
   TIM_Cmd(TIM1, ENABLE);
 }

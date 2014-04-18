d778dc77ec44cbe05daaf2d205595c59791bccad
Joe Ferner <joe@fernsroth.com>
2014-04-18 15:18:54 -0400

    fix build

--- firmware/build.sh
+++ firmware/build.sh
@@ -2,6 +2,6 @@
 
 mkdir -p build
 cd build
-rm stm32-ft312d-test.elf || echo "Cannot remove. stm32-ft312d-test.elf not build?"
-make stm32-ft312d-test.bin && \
-make stm32-ft312d-test.list
+rm stm32-rfid-clone.elf || echo "Cannot remove. stm32-rfid-clone.elf not build?"
+make stm32-rfid-clone.bin && \
+make stm32-rfid-clone.list
